915968c506b46f27576412e0b3459f4f80c28f7a
MengyingYang <ystrickland@163.com>
2017-05-21 08:41:40 +0800

    layout

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,13 @@
  * Created by Mengying on 2017/5/18.
  */
 $(function(){
-        $("#image-a").attr("data-image-src","images/1.jpg");
-        $("#image-b").attr("data-image-src","images/xiongdazhen.jpg");
-        $("#image-c").attr("data-image-src","images/denjiaxian.png");
+        $("#image-banner").attr("data-image-src","images/1.jpg");
+        $("#image-a").attr("data-image-src","images/timg.jpg");
+        $("#image-b").attr("data-image-src","images/xiongdazhen2.jpg");
+        $("#image-c").attr("data-image-src","images/dengjiaxian3.jpg");
+        $("#image-d").attr("data-image-src","images/shiye.jpg");
+        $("#image-e").attr("data-image-src","images/city2.jpg");
+
 
     if (navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/)) {
         $('#ios-notice').removeClass('hidden');
