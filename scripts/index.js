38e13cb3da69d09aacefcd86e9b826f2bcf65840
MengyingYang <ystrickland@163.com>
2017-05-21 17:05:07 +0800

    chenmenglei

--- scripts/index.js
+++ scripts/index.js
@@ -15,7 +15,7 @@ $(function(){
         $('.parallax-container').height( $(window).height() * 0.5 | 0 );
     } else {
         $(window).resize(function(){
-            var parallaxHeight = Math.max($(window).height() * 0.7, 200) | 0;
+            var parallaxHeight = Math.max($(window).height() * 1, 200) | 0;
             $('.parallax-container').height(parallaxHeight);
         }).trigger('resize');
     }
